1b551535bca1a82f514ec5626a18ff26521c9816
TestForEducation <31298729+TestForEducation@users.noreply.github.com>
2017-08-25 08:34:17 +0800

    Delete client.bat

--- client.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-CD /D %~dp0
-
-setLocal EnableDelayedExpansion
-for /R .\lib %%A in (*.jar) do (
-   set JARPATH=%%A;!JARPATH!
-)
-@START /B jre\bin\javaw -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8 -Duser.timezone=Asia/Shanghai -Xms256M -Xmx256M -Xmn192M  -classpath !JARPATH!.\bin\ cross.wall.Windows >> client.log
